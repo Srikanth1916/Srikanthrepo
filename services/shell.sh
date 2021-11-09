3609f028b55ff05132fad617ddb44ca2189d0e41
Srikanth1916 <39981109+Srikanth1916@users.noreply.github.com>
2021-11-09 17:49:12 +0530

    Update shell.sh

--- services/shell.sh
+++ services/shell.sh
@@ -12,7 +12,7 @@ C=`grep ${!pro} ecr.txt`
 if [ -z  "$C" ]
 then
 echo "creating the repoistory for ${!pro}"
-aws ecr create-repository --repository-name ${!pro} --image-scanning-configuration scanOnPush=false --region ca-central-1
+aws ecr create-repository --repository-name ${!pro} --image-scanning-configuration scanOnPush=false --region us-east-2
 else
 echo "Match found"
 fi
